cb76b45013ac903fb5593c3fe7f7020f26a313e3
keriyaman <villavilla1234567890987654321@gmail.com>
2025-06-17 07:27:38 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Session id",
+SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=l4wggTSQ#6lpo3LME0GBvWQop7tRwaeD6cn0MSvkBotOAj6lT4eg",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
